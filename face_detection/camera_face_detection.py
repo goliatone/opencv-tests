8366fd56e486ea0c1869a6509af6333ac5b17833
goliatone <burgosemi@gmail.com>
2015-01-21 13:10:32 -0500

    update comments

--- face_detection/camera_face_detection.py
+++ face_detection/camera_face_detection.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
 import cv2
 import cv2.cv as cv
-import sys
-import time
 
+
+#TODO: We should check file exists...YOLO!
 cascade = cv2.CascadeClassifier('./haarcascade_frontalface_alt.xml')
+
 
 def face_detect(img, cascade):
     rects = cascade.detectMultiScale(img, scaleFactor=1.1,
@@ -12,12 +14,15 @@ def face_detect(img, cascade):
                                     flags = cv.CV_HAAR_SCALE_IMAGE)
     if len(rects) == 0:
         return []
-    rects[:,2:] += rects[:,:2]
+    rects[:, 2:] += rects[:, :2]
     return rects
 
+
 def draw_rects(img, rects, color):
-    for x1, y1, x2, y2 in rects:
-        cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
+
+    for (x, y, w, h) in rects:
+        cv2.rectangle(img, (x, y), (w, h), color, 2)
+
 
 #Open camera and set dimensions
 cam = cv2.VideoCapture(0)
@@ -35,7 +40,6 @@ ret, img = cam.read()
 while cam.isOpened():
     #image
     ret, img = cam.read()
-
 
     gray = cv2.cvtColor(img, cv.CV_BGR2GRAY)
     gray = cv2.equalizeHist(gray)
